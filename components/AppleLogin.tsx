5ffabb6c02e921a050ba975b7c571b69055da0a0
divinejoshua <divineuche399@gmail.com>
2024-06-07 20:45:24 +0100

    Updated app.json

--- components/AppleLogin.tsx
+++ components/AppleLogin.tsx
@@ -34,7 +34,25 @@ export default function AppleLoginComponent() {
         buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
         cornerRadius={5}
         style={styles.button}
-        onPress={loginWithApple}
+        onPress={
+          async () => {
+            try {
+              const credential = await AppleAuthentication.signInAsync({
+                requestedScopes: [
+                  AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+                  AppleAuthentication.AppleAuthenticationScope.EMAIL,
+                ],
+              });
+              // signed in
+            } catch (e :any) {
+              if (e.code === 'ERR_REQUEST_CANCELED') {
+                // handle that the user canceled the sign-in flow
+              } else {
+                // handle other errors
+              }
+            }
+          }
+        }
         />
     </ThemedView>
   )
